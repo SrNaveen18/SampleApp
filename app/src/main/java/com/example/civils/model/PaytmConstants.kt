28b1f8a8e2bcdd4e305eb25b7f8d49003b319e96
SrNaveen18 <naveenshades18@gmail.com>
2019-01-30 18:30:39 +0530

    After PG Integration

---
+++ app/src/main/java/com/example/civils/model/PaytmConstants.kt
@@ -0,0 +1,17 @@
+package com.example.civils.model
+
+class PaytmConstants {
+    companion object {
+        const val M_ID = "rxazcv89315285244163" //Paytm Merchand Id we got it in paytm credentials
+        const val CHANNEL_ID = "WAP" //Paytm Channel Id, got it in paytm credentials
+        const val INDUSTRY_TYPE_ID = "Retail" //Paytm industry type got it in paytm credential
+
+        const val WEBSITE = "APPSTAGING"
+        const val CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp"
+
+       // const val ORDER_ID ="ORDERID_456760TT"
+        const val CUST_ID = "CSAM563210"
+
+        const val AMOUNT = "100.0"
+    }
+}
